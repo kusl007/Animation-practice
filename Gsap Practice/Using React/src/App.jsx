37cd8cd171e3304353606fbe6f4bc47f2bc7e48d
kusl007 <kushal.bansal.1001@gmail.com>
2024-08-16 21:12:29 +0545

    gsap with react started

--- Gsap Practice/Using React/src/App.jsx
+++ Gsap Practice/Using React/src/App.jsx
@@ -1,7 +1,20 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./components/Homepage/index"
+
+  import React from 'react'
+  
+  const App = () => {
+    return (
+      <div>
+    <nav className="bg-slate-400 w-screen h-20">
+      <Link to="/">Home</Link>
+    </nav>
+    <Routes>
+      <Route to="/" element={<Home/>}/>
+
+    </Routes>
+   </div>
+    )
+  }
+  
+  export default App
---
+++ Gsap Practice/Using React/src/components/Homepage/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div className='w-full h-full'>
+        <p className='text-5xl  text-white'>
+
+        Welcome to Home page
+        </p>
+
+    </div>
+  )
+}
+
+export default index
